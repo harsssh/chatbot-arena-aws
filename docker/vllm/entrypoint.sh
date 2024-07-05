eb553dff15ebddeb983d0d914cc75c8c9ab62052
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-07-05 19:09:38 +0900

    entrypoint.sh を更新

--- docker/vllm/entrypoint.sh
+++ docker/vllm/entrypoint.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -ex
 : "${MODEL:?}"
-: "${PORT:?}" 
-: "${TENSOR_PARALLEL_SIZE:?}"
+: "${PORT:?}"
+: "${CORES:?}"
 
-export NEURON_RT_VISIBLE_CORES=${NEURON_RT_VISIBLE_CORES:-None}
+export NEURON_RT_VISIBLE_CORES="$CORES"
 
-options="--model ${MODEL} \
-    --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} \
-    --max-num-seqs ${MAX_NUM_SEQS:-8} \
-    --max-model-len ${MAX_MODEL_LEN:-4096} \
-    --block-size ${MAX_MODEL_LEN:-4096} \
-    --port ${PORT} \
-    --host 0.0.0.0 \
-    --disable-log-stats"
-
-if [ -n "${CHAT_TEMPLATE}" ]; then
-    options="${options} --chat-template ${CHAT_TEMPLATE}"
+if [ -z "${TENSOR_PARALLEL_SIZE}" ]; then
+    TENSOR_PARALLEL_SIZE=$(echo "${CORES}" | awk -F- '{print $2 - $1 + 1}')
 fi
 
-python3 -m vllm.entrypoints.openai.api_server ${options}
+options=(
+    --model "${MODEL}"
+    --tensor-parallel-size "${TENSOR_PARALLEL_SIZE}"
+    --max-num-seqs "${MAX_NUM_SEQS:-8}"
+    --max-model-len "${MAX_MODEL_LEN:-4096}"
+    --block-size "${MAX_MODEL_LEN:-4096}"
+    --port "${PORT}"
+    --host 0.0.0.0
+    --disable-log-stats
+)
+
+if [ -n "${CHAT_TEMPLATE}" ]; then
+    options+=(--chat-template "${CHAT_TEMPLATE}")
+fi
+
+python3 -m vllm.entrypoints.openai.api_server "${options[@]}"
